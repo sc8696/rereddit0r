593d79689bb39d0d6bb9512227614791e991fdda
Steve <evenstevens280@gmail.com>
2015-01-20 20:18:21 +0000

    Update reddit0r.js
    Updated to not depend on zepto

--- package/reddit0r.js
+++ package/reddit0r.js
@@ -1,5 +1,14 @@
-$('img').each(function(){
-    if($(this).attr('src').indexOf('heavy-load.png') !== -1){
-      location.reload();
-    }
-})
+var images = document.getElementsByTagName("img");
+
+var whatToLookOutFor = [
+	'heavy-load.png',
+	'trouble-afoot.png'
+]
+
+for(var i = 0; i < images.length; i+=1){
+	for(var j = 0; j < whatToLookOutFor.length; j+=1){
+		if(images[i].src.indexOf(whatToLookOutFor[j]) !== -1){
+			location.reload();
+		} 
+	}
+}
